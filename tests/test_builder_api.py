f3b90c0773b755de21342423a994f4ca14562b2a
KsyuSmile <nezy_ksuy@mail.ru>
2023-06-05 17:40:01 +0400

    update

--- tests/test_builder_api.py
+++ tests/test_builder_api.py
@@ -18,11 +18,12 @@ def test_get_list_size(builder_get, model_type):
 
 
 """Получение списка размеров сумок, некорректные данные"""
-@pytest.mark.parametrize("model_type", ['', '123', 'Gjdks', 123654, '_-/?%*'])
+@pytest.mark.parametrize("model_type", ['', 'лжрыАЫРииыооОЫ', '123', 'Gjdks', 123654, '_-/?%*'])
 def test_get_list_size_invalid_type(builder_get, model_type):
     status, result = builder_get.list_size(model_type)
     assert status == 400
     print(status)
+    # Тест не пройден
 
 
 """Получение сумки, позитивные тесты с корректными данными"""
@@ -51,7 +52,6 @@ def test_get_get_leather_mini(builder_get, model_type='leather', size='mini'):
     assert 'parameters' in result
     assert 'accessories' in result
     assert status == 200
-    print(result)
 
 
 def test_get_get_s_pinami_maxi(builder_get, model_type='osnova-s-pinami', size='maxi'):
@@ -79,7 +79,6 @@ def test_get_get_s_pinami_mini(builder_get, model_type='osnova-s-pinami', size='
     assert 'parameters' in result
     assert 'accessories' in result
     assert status == 200
-    print(result)
 
 
 def test_get_get_plastic_maxi(builder_get, model_type='plastic', size='maxi'):
@@ -137,50 +136,85 @@ def test_get_get_puffed_maxi(builder_get, model_type='steganaya-tkan', size='max
 
 
 """Получение сумки, негативные тесты с некорректными данными"""
-@pytest.mark.parametrize("size", ['', '649786', 'ipRthOjh', 123654, '?,@%(*'])
+@pytest.mark.parametrize("size", ['', 'ФнвОЫУисллпП', '649786', 'ipRthOjh', 123654, '?,@%(*'])
 def test_get_get_leather_invalid_size(builder_get, size, model_type='leather'):
     """Кожаная основа, некорректный размер"""
     status, result = builder_get.get_bag(model_type, size)
     print(result)
     assert 'error' in result
     assert status == 400
+    # Тест не пройден
 
 
-@pytest.mark.parametrize("size", ['', '649786', 'ipRthOjh', 123654, '?,@%(*'])
+@pytest.mark.parametrize("size", ['', 'ФнвОЫУисллпП', '649786', 'ipRthOjh', 123654, '?,@%(*'])
 def test_get_get_plastic_invalid_size(builder_get, size, model_type='plastic'):
     """Прозрачная основа, некорректный размер"""
     status, result = builder_get.get_bag(model_type, size)
     print(result)
     assert 'error' in result
     assert status == 400
+    # Тест не пройден
 
 
-@pytest.mark.parametrize("size", ['', '649786', 'ipRthOjh', 123654, '?,@%(*'])
+@pytest.mark.parametrize("size", ['', 'ФнвОЫУисллпП', '649786', 'ipRthOjh', 123654, '?,@%(*'])
 def test_get_get_s_pinami_invalid_size(builder_get, size, model_type='osnova-s-pinami'):
     """Основа с пинами, некорректный размер"""
     status, result = builder_get.get_bag(model_type, size)
     print(result)
     assert 'error' in result
     assert status == 400
+    # Тест не пройден
 
 
-@pytest.mark.parametrize("size", ['', '649786', 'ipRthOjh', 123654, '?,@%(*'])
+@pytest.mark.parametrize("size", ['', 'ФнвОЫУисллпП', '649786', 'ipRthOjh', 123654, '?,@%(*'])
 def test_get_get_mix_invalid_size(builder_get, size, model_type='plastic_and_leather'):
     """Mix основа, некорректный размер"""
     status, result = builder_get.get_bag(model_type, size)
     print(result)
     assert 'error' in result
     assert status == 400
+    # Тест не пройден
 
 
-@pytest.mark.parametrize("size", ['', '649786', 'ipRthOjh', 123654, '?,@%(*'])
+@pytest.mark.parametrize("size", ['', 'ФнвОЫУисллпП', '649786', 'ipRthOjh', 123654, '?,@%(*'])
 def test_get_get_puffed_invalid_size(builder_get, size, model_type='steganaya-tkan'):
     """Puffed основа, некорректный размер"""
     status, result = builder_get.get_bag(model_type, size)
     print(result)
     assert 'error' in result
     assert status == 400
+    # Тест не пройден
 
 
+"""Добавление сумки в корзину с корректными данными"""
+def test_get_add_bag_to_cart(builder_post, action='builder_add_to_cart', components_id='["5283","17653","4353"]', components='{"sections":{"osnova":{"pa_base_logo":{"png":"https://askstudio.staging.juzt.studio/app/uploads/2022/07/logo-png-mask-1.png","svg":"https://askstudio.staging.juzt.studio/wp-json/juzt-ask/v1/builder-image?url=https://askstudio.staging.juzt.studio/app/uploads/2022/07/maxi-logo-plastic-2.svg","positionX":0,"positionY":572,"zIndex":101,"backgroundFull":"https://askstudio.staging.juzt.studio/app/uploads/2022/07/pink-logo-maxi.png"}},"meshok":{"pa_bag_color":{"png":"https://askstudio.staging.juzt.studio/app/uploads/2022/06/transparent-base-maxi-bag-1.png","svg":"https://askstudio.staging.juzt.studio/wp-json/juzt-ask/v1/builder-image?url=https://askstudio.staging.juzt.studio/app/uploads/2022/06/transparent-base-maxi-bag-1.svg?v=1685771965","positionX":0,"positionY":517,"zIndex":1,"backgroundFull":"https://askstudio.staging.juzt.studio/app/uploads/2023/04/p1.jpg"}},"remen":{"pa_bagstrap_color":{"png":"https://askstudio.staging.juzt.studio/app/uploads/2022/07/transparent-base-maxi-bagstrap-lanyard.png","svg":"https://askstudio.staging.juzt.studio/wp-json/juzt-ask/v1/builder-image?url=https://askstudio.staging.juzt.studio/app/uploads/2022/07/transparent-base-maxi-bagstrap-lanyard.svg?v=1685771965","positionX":0,"positionY":42,"zIndex":1,"backgroundFull":"https://askstudio.staging.juzt.studio/app/uploads/2022/06/bagstrap-lanyard-black-500h500.jpg"}}},"accessories":{},"parameters":{"width":600,"height":1183}}'):
+    status, result = builder_post.add_bag_to_cart(action, components_id, components)
+    assert 'cart_hash' in result
+    assert 'fragments' in result
+    assert status == 200
 
 
+"""Добавление сумки в корзину с некорректными данными"""
+@pytest.mark.parametrize("action", ['', 'проыЬлЗЦоор', '6548116846', 'tYpoqFhKjah', 97613245654, '?,@%(*'])
+def test_get_add_bag_to_cart_invalid_action(builder_post, action, components_id='["5283","17653","4353"]', components='{"sections":{"osnova":{"pa_base_logo":{"png":"https://askstudio.staging.juzt.studio/app/uploads/2022/07/logo-png-mask-1.png","svg":"https://askstudio.staging.juzt.studio/wp-json/juzt-ask/v1/builder-image?url=https://askstudio.staging.juzt.studio/app/uploads/2022/07/maxi-logo-plastic-2.svg","positionX":0,"positionY":572,"zIndex":101,"backgroundFull":"https://askstudio.staging.juzt.studio/app/uploads/2022/07/pink-logo-maxi.png"}},"meshok":{"pa_bag_color":{"png":"https://askstudio.staging.juzt.studio/app/uploads/2022/06/transparent-base-maxi-bag-1.png","svg":"https://askstudio.staging.juzt.studio/wp-json/juzt-ask/v1/builder-image?url=https://askstudio.staging.juzt.studio/app/uploads/2022/06/transparent-base-maxi-bag-1.svg?v=1685771965","positionX":0,"positionY":517,"zIndex":1,"backgroundFull":"https://askstudio.staging.juzt.studio/app/uploads/2023/04/p1.jpg"}},"remen":{"pa_bagstrap_color":{"png":"https://askstudio.staging.juzt.studio/app/uploads/2022/07/transparent-base-maxi-bagstrap-lanyard.png","svg":"https://askstudio.staging.juzt.studio/wp-json/juzt-ask/v1/builder-image?url=https://askstudio.staging.juzt.studio/app/uploads/2022/07/transparent-base-maxi-bagstrap-lanyard.svg?v=1685771965","positionX":0,"positionY":42,"zIndex":1,"backgroundFull":"https://askstudio.staging.juzt.studio/app/uploads/2022/06/bagstrap-lanyard-black-500h500.jpg"}}},"accessories":{},"parameters":{"width":600,"height":1183}}'):
+    """Некорректный action"""
+    status, result = builder_post.add_bag_to_cart(action, components_id, components)
+    assert status == 400
+
+
+@pytest.mark.parametrize("components_id", ['', 'проыЬлЗЦоор', '6548116846', 'tYpoqFhKjah', 97613245654, '?,@%(*'])
+def test_get_add_bag_to_cart_invalid_components_id(builder_post, components_id, action='builder_add_to_cart', components='{"sections":{"osnova":{"pa_base_logo":{"png":"https://askstudio.staging.juzt.studio/app/uploads/2022/07/logo-png-mask-1.png","svg":"https://askstudio.staging.juzt.studio/wp-json/juzt-ask/v1/builder-image?url=https://askstudio.staging.juzt.studio/app/uploads/2022/07/maxi-logo-plastic-2.svg","positionX":0,"positionY":572,"zIndex":101,"backgroundFull":"https://askstudio.staging.juzt.studio/app/uploads/2022/07/pink-logo-maxi.png"}},"meshok":{"pa_bag_color":{"png":"https://askstudio.staging.juzt.studio/app/uploads/2022/06/transparent-base-maxi-bag-1.png","svg":"https://askstudio.staging.juzt.studio/wp-json/juzt-ask/v1/builder-image?url=https://askstudio.staging.juzt.studio/app/uploads/2022/06/transparent-base-maxi-bag-1.svg?v=1685771965","positionX":0,"positionY":517,"zIndex":1,"backgroundFull":"https://askstudio.staging.juzt.studio/app/uploads/2023/04/p1.jpg"}},"remen":{"pa_bagstrap_color":{"png":"https://askstudio.staging.juzt.studio/app/uploads/2022/07/transparent-base-maxi-bagstrap-lanyard.png","svg":"https://askstudio.staging.juzt.studio/wp-json/juzt-ask/v1/builder-image?url=https://askstudio.staging.juzt.studio/app/uploads/2022/07/transparent-base-maxi-bagstrap-lanyard.svg?v=1685771965","positionX":0,"positionY":42,"zIndex":1,"backgroundFull":"https://askstudio.staging.juzt.studio/app/uploads/2022/06/bagstrap-lanyard-black-500h500.jpg"}}},"accessories":{},"parameters":{"width":600,"height":1183}}'):
+    """Некорректный components_id"""
+    status, result = builder_post.add_bag_to_cart(action, components_id, components)
+    print(result)
+    assert status == 400
+    # Тест не пройден
+
+
+@pytest.mark.parametrize("components", ['', 'проыЬлЗЦоор', '6548116846', 'tYpoqFhKjah', 97613245654, '?,@%(*'])
+def test_get_add_bag_to_cart_invalid_components_id(builder_post, components, action='builder_add_to_cart', components_id='["5283","17653","4353"]'):
+    """Некорректный components"""
+    status, result = builder_post.add_bag_to_cart(action, components_id, components)
+    print(result)
+    assert status == 400
+    # Тест не пройден
